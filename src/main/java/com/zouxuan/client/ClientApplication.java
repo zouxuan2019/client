ee775ec6fbb1a917f5b4b712c24ae32c2778e0e8
zouxuan <zouxuan@dbs.com>
2020-03-25 23:22:27 +0800

    Fix scanBase packages issue

--- src/main/java/com/zouxuan/client/ClientApplication.java
+++ src/main/java/com/zouxuan/client/ClientApplication.java
@@ -3,7 +3,7 @@ package com.zouxuan.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.zouxuan.auth", "com.zouxuan.client"})
 public class ClientApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/zouxuan/client/ClientController.java
+++ src/main/java/com/zouxuan/client/ClientController.java
@@ -1,18 +1,22 @@
 package com.zouxuan.client;
 
 import com.zouxuan.auth.PermissionScope;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@EnableAutoConfiguration
 public class ClientController {
     @GetMapping("/test")
     @PermissionScope("admin")
     public void authTest() {
         System.out.println( "aaa" );
+
+    }
+
+    @GetMapping("/anon/test")
+    public void authTest2() {
+        System.out.println( "mmm" );
 
     }
 }
